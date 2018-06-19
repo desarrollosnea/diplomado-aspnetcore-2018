fc9736e8ab3fb5ad001f7ac9e4e4acaa094122bc
Jose A. Fernandez <fernandezja@gmail.com>
2018-06-19 19:55:25 -0300

    Cambio de texto

--- Unidad 1/ConsoleApp1/Program.cs
+++ Unidad 1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Diplomado ASP.NET Core 2.1!");
 
             Console.ReadKey();
         }
