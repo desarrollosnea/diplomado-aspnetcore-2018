7918f3c46f13fb91a59ab2750ca3cb1c94f041fc
Jose A. Fernandez <fernandezja@gmail.com>
2018-06-19 20:16:00 -0300

    Web App de ejemplo para Response

--- Unidad 1/ConsoleApp1/Program.cs
+++ Unidad 1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Diplomado ASP.NET Core 2.1!");
+            Console.WriteLine("Hola Diplomado ASP.NET Core 2.1.0!");
 
             Console.ReadKey();
         }
