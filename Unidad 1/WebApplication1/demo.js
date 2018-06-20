e2505463fbc13de96ad2804548fca0dccb111dbc
Jose A. Fernandez <fernandezja@gmail.com>
2018-06-19 22:29:05 -0300

    ejemplo con javascript, metodos app.metodo1 y app.comunes.metodo2 en modulos. Separacion de archivo .js

---
+++ Unidad 1/WebApplication1/demo.js
@@ -0,0 +1,17 @@
+﻿var app = app || {};
+
+//app.metodo2 = function(param1) {
+//    return param1 + ' (dentro de metodo 2)';
+//};
+
+app.comunes = function () {
+
+    var self = {};
+
+    self.metodo2 = function (param1) {
+        return param1 + ' (dentro de comunes.metodo2)';
+    }
+
+    return self;
+
+}();
