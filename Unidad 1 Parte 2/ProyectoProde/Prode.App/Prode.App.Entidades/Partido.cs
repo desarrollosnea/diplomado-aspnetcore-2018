a8f73465b568fd04d6de6ba664e5995238f56510
melanie.ernst <melanie.ernst@NBDN14>
2018-06-27 15:33:15 -0300

    Proyecto MVC

---
+++ Unidad 1 Parte 2/ProyectoProde/Prode.App/Prode.App.Entidades/Partido.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prode.App.Entidades
+{
+	public class Partido
+	{
+		public Equipo EquipoLocal { get; set; }
+		public Equipo EquipoVisitante { get; set; }
+		public int GolesLocal { get; set; }
+		public int GolesVisitante { get; set; }
+		public DateTime Fecha { get; set; }
+		public string Lugar { get; set; }
+
+		public string InformarResultado
+		{
+			get
+			{
+				var resultado = "";
+				if (GolesVisitante > GolesLocal)
+				{
+					resultado = "Ganó " + EquipoVisitante.Nombre;
+				}
+				else if (GolesVisitante < GolesLocal)
+				{
+					resultado = "Ganó " + EquipoLocal.Nombre;
+				}
+				else
+				{
+					resultado = "Empate";
+				}
+				return resultado;
+			}
+			
+		}
+	}
+}
---
+++ Unidad 1 Parte 2/ProyectoProde/Prode.App/Prode.App.Entidades/Resultados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Prode.App.Entidades.Enumeraciones
+{
+	public enum ResultadoEnum : byte
+	{
+		[Description("Empate")]
+		empate = 0,
+		[Description("Ganó")]
+		gano = 1,
+		[Description("Perdio")]
+		perdio = 2,
+	}
+}
+
+
