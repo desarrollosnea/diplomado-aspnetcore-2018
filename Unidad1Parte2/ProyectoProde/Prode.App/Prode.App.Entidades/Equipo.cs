18037ddd1555027b0e1c9face38871bdd6f678b4
melanie.ernst <melanie.ernst@NBDN14>
2018-06-27 15:45:06 -0300

    Unidad1Parte2

---
+++ Unidad1Parte2/ProyectoProde/Prode.App/Prode.App.Entidades/Equipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prode.App.Entidades
+{
+    public class Equipo
+    {
+		public int EquidoId { get; set; }
+
+		public string Nombre { get; set; }
+
+	}
+}
---
+++ Unidad1Parte2/ProyectoProde/Prode.App/Prode.App/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prode.App.Models;
+
+namespace Prode.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+		//Pruebas con bootstrap
+		public IActionResult ProbandoBootstrap()
+		{
+			ViewBag.Mostrar = true;
+			return View();
+		}
+    }
+}
