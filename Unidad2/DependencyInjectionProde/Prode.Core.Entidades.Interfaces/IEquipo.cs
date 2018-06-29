17d75c196fe571ed1dcbff366ef530c84ea3f22d
Jose A. Fernandez <fernandezja@gmail.com>
2018-06-28 21:17:45 -0300

    Dependency Injection con Microsoft.Extensions.DependencyInjection

---
+++ Unidad2/DependencyInjectionProde/Prode.Core.Entidades.Interfaces/IEquipo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Prode.Core.Entidades.Interfaces
+{
+    public interface IEquipo
+    {
+        string Nombre { get; set; }
+        string Abreviatura { get; set; }
+    }
+}
---
+++ Unidad2/DependencyInjectionProde/ProdeWebApp/Controllers/EquipoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prode.Core.Entidades;
+using Prode.Core.Entidades.Interfaces;
+
+namespace ProdeWebApp.Controllers
+{
+    public class EquipoController : Controller
+    {
+        private IFormateador _formateador;
+
+        public EquipoController(IFormateador formateador,)
+        {
+            _formateador = formateador;
+        }
+
+        public IActionResult Index()
+        {
+            var argentina = new Equipo()
+            {
+                Nombre = "Argentina",
+                Abreviatura = "ARG"
+            };
+
+
+            ViewData["Nombre"] = _formateador.NombreCompleto(argentina);
+
+            return View();
+        }
+    }
+}
