40c78c6297a3dd3c1929c5871912b581f68bb0cf
Jose A. Fernandez <fernandezja@gmail.com>
2018-07-10 22:02:05 -0300

    Environment

---
+++ Unidad2/Environment/Environment01/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Environment01.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Environment01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _env { get; set; }
+
+        public HomeController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Ambiente = _env.EnvironmentName;
+            ViewBag.IsDevelopment = _env.IsDevelopment();
+            ViewBag.IsStaging = _env.IsStaging();
+            ViewBag.IsProduction = _env.IsProduction();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unidad2/Environment/Environment01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Environment01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
